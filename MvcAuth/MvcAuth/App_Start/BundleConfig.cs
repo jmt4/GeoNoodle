5968a3351738533a67c0710868ad909e8c3bedea
jmt4 <jimmtriphahn@gmail.com>
2015-09-04 19:31:20 -0700

    Zocial css for pretty social login buttons

--- MvcAuth/MvcAuth/App_Start/BundleConfig.cs
+++ MvcAuth/MvcAuth/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace MvcAuth
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/zocial.css"));
         }
     }
 }
