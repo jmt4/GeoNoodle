87957b98ddc1ed313bdc26aeea8b143bf5219086
jmt4 <jimmtriphahn@gmail.com>
2015-09-13 12:13:35 -0700

    MVC for pay stats

---
+++ MvcAuth/MvcAuth/Controllers/PayStatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAuth.Controllers
+{
+    public class PayStatisticsController : Controller
+    {
+        /* ASP.NET MVC uses the format below to determine what code to invoke */
+        /* /[Controller]/[ActionName]/[Parameters] */
+        /* https://localhost:44300/PayStatistics/Index */
+        /* https://localhost:44300/PayStatistics/Welcome */
+        // GET: PayStatistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
---
+++ MvcAuth/MvcAuth/Models/Wages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAuth.Models
+{
+    public class Wages
+    {
+        public DateTime date { get; set; }
+        public double wage { get; set; }
+    }
+}
