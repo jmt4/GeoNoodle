94dc86d5763f59541b4c8c575934408b912d997d
jmt4 <jimmtriphahn@gmail.com>
2015-09-20 20:44:21 -0700

    models for each statistic graph

---
+++ MvcAuth/MvcAuth/Models/StatisticsViewModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace MvcAuth.Models
+{
+    public enum Category
+    {
+        Science, Technology, Trade, Health
+    }
+    public class Job
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public int Top10Pay { get; set; }
+        public int MedianPay { get; set; }
+        public int Bottom10Pay { get; set; }
+        /* Added for job Density stats page */
+        /* Create Density models -- County Value Job */
+        public virtual ICollection<Density> Densities { get; set; }
+        /* Added for trending jobs */
+        /* Create JobCount model -- Year Count Job */
+        public virtual ICollection<JobCount> JobCounts { get; set; }
+    }
+
+    public class StatisticsDBContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Density> Densities { get; set; }
+        public DbSet<JobCount> JobCounts { get; set; }
+    }
+}
